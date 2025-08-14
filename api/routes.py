1272f3a10d605fbb37b11ac8bacca1597c346932
AlisonSabanP <alisonsabanpiche@gmail.com>
2025-08-14 03:22:16 -0600

    Clase automata: se añadieron más validaciones de transiciones y se realizo la función para procesar una cadena y validar las cadenas de prueba. Se agregó una funcionalidad que muestra el error por el cual el automata es invalido
    routes.py: se añadieron validaciones para las claves del diccionario, y se muestran los errores

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.automata import Automata 
+from app.automata import Automata
 import json
 
 api = Blueprint('api', __name__)
@@ -16,30 +16,42 @@ def process_automata():
 
     try:
         data = json.load(file)
+        print(data)
     except Exception as e:
         return jsonify({"error": f"Error al leer JSON: {str(e)}"}), 400
 
     if not isinstance(data, list):
         return jsonify({"error": "El JSON debe ser una lista de autómatas"}), 400
 
-    results = []  
+    results = []
+    required_fields = ["id","name","initial_state","acceptance_states","alphabet","states","transitions","test_strings"]
 
     for automaton_data in data:
         if not isinstance(automaton_data, dict):
             return jsonify({"error": "Cada autómata debe ser un diccionario"}), 400
 
-        automaton = Automata(automaton_data)
+        missing_fields = [field for field in required_fields if field not in automaton_data]
+        if missing_fields:
+            results.append({
+                "id": automaton_data.get("id", None),
+                "success": False,
+                "error": f"Faltan campos obligatorios: {', '.join(missing_fields)}"
+            })
+            continue
 
+        automaton = Automata(automaton_data)
         is_valid = automaton.validate()
 
         result = {"id": automaton.id}
 
         if not is_valid:
             result["success"] = False
+            result["error"] = automaton.error
         else:
             result["success"] = True
-
+            result["inputs_validation"] = automaton.validate_inputs()
+        
         results.append(result)
 
-    return jsonify(results), 200
 
+    return jsonify(results), 200
--- app/automata.py
+++ app/automata.py
@@ -7,28 +7,71 @@ class Automata():
         self.alphabet = data_automata['alphabet']
         self.states = data_automata['states']
         self.transitions = data_automata['transitions']
-        self.test_string = data_automata.get('test_strings', [])
+        self.test_strings = data_automata.get('test_strings', [])
+        self.error = ''
+        self.is_valid = False
+
     
     def validate(self):
-        required_fields = ["id", "name", "initial_state", "acceptance_states", "alphabet", "states", "transitions"]
-        for field in required_fields:
-            if not hasattr(self, field) or getattr(self, field) is None:
-                return False
-
         if not self.initial_state:
+            self.error = 'No existe estado inicial para el automata.'    
             return False
 
         if self.initial_state not in self.states:
+            self.error = f'El estado inicial {self.initial_state} no está en la lista de estados'
             return False
         
         if not self.acceptance_states:
+            self.error = 'No existen estados de aceptación en automata.'
             return False
         
         for state in self.acceptance_states:
             if state not in self.states:
+                self.error = f'El estado de aceptación {state} no existe entre los estados validos.'
                 return False
                 
         if len(self.alphabet) == 0:
+            self.error = 'En el automata no existe un alfabeto.'
             return False
+        
+        for transition in self.transitions:
+            symbol = transition['symbol']
+            if symbol not in self.alphabet:
+                self.error = f'El carácter {symbol} no está definido en el alfabeto del autómata'
+                return False
 
+            if transition['from_state'] not in self.states or transition['to_state'] not in self.states:
+                self.error = 'Algunos de los estados en las transiciones no están definidos'
+                return False
+
+        for state in self.states:
+            has_transition = any(t['from_state'] == state for t in self.transitions)
+            if not has_transition:
+                self.error = f'El estado {state} no tiene transiciones definidas'
+                return False
+        
+        self.is_valid = True
         return True
+
+    def process_string(self, string):
+        state = self.initial_state
+        for character in string:
+            valid = False
+            for transition in self.transitions:
+                if transition["from_state"] == state and transition["symbol"] == character:
+                    state = transition["to_state"]
+                    valid = True
+                    break
+            if not valid:
+                return False
+        for acc_state in self.acceptance_states:
+            if state == acc_state:
+                return True
+        return False
+
+    def validate_inputs(self):
+        results = []
+        for string in self.test_strings:
+            result = self.process_string(string)
+            results.append({"string": string, "resultado": result})
+        return results
